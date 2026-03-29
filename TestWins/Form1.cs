a8259945850b7384dd0b7d017684eaad51f172f8
JoshyyCoded <largadojoshhunter@gmail.com>
2026-03-29 15:35:01 +0800

    Update Form1.cs

--- TestWins/Form1.cs
+++ TestWins/Form1.cs
@@ -6,38 +6,67 @@ namespace TestWins;
 public partial class Form1 : Form
 {
     //business
-
     private readonly StudentController controller = new StudentController();
+
     public Form1()
     {
         InitializeComponent();
-        loadData();
-    }
-
-    private void loadData()
-    {
-        dataGridView1.DataSource = controller.getAll();
     }
 
     private void btnAdd_Click(object sender, EventArgs e)
     {
+        var student = new TestWins.Model.Student
+        {
+            studentId = txtStudentId.Text,
+            Name = txtName.Text,
+            age = int.Parse(txtAge.Text),
+            course = txtCourse.Text
+        };
 
+        controller.createStudent(student);
+        loadData();
+        clearFields();
     }
 
     private void btnUpdate_Click(object sender, EventArgs e)
     {
+        var student = new TestWins.Model.Student
+        {
+            studentId = txtStudentId.Text,
+            Name = txtName.Text,
+            age = int.Parse(txtAge.Text),
+            course = txtCourse.Text
+        };
 
+        controller.update(student);
+        loadData();
+        clearFields();
     }
 
     private void btnDelete_Click(object sender, EventArgs e)
     {
-
-
-
+        controller.delete(txtStudentId.Text);
+        loadData();
+        clearFields();
     }
 
     private void dataGridView1_CellClick(object sender, EventArgs e)
     {
+        if (dataGridView1.CurrentRow != null)
+        {
+            DataGridViewRow row = dataGridView1.CurrentRow;
+            txtStudentId.Text = row.Cells[0].Value.ToString();
+            txtName.Text = row.Cells[1].Value.ToString();
+            txtAge.Text = row.Cells[2].Value.ToString();
+            txtCourse.Text = row.Cells[3].Value.ToString();
+        }
+    }
 
+    private void clearFields()
+    {
+        txtStudentId.Text = "";
+        txtName.Text = "";
+        txtAge.Text = "";
+        txtCourse.Text = "";
     }
 }
